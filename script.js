f200152fe682aa8a99df02f4496d7d45dd6e6782
evelynolofin <olofinevelyn@gmail.com>
2025-05-09 08:36:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = [
+    "eve", "eri", "david"
+]
+
+console.log(name)
+
+let scoreboard = [23,14,7,0,78,95,36]
+
+console.log(scoreboard)
+
+let studentId = {
+    name: "Evelyn",
+    age: 45,
+    score: 80,
+    surname: "Olofin"
+}
+
+console.log(studentId)
